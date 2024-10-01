75796d25d978adeb3ad67801e4e9a50779cd6235
HemaHarini <118076238+HemaHariniG@users.noreply.github.com>
2024-10-01 23:05:49 +0530

    Update TDS.ino

--- TDS.ino
+++ TDS.ino
@@ -1,5 +1,5 @@
-#define TdsSensorPin 25
-#define VREF 5.0              // analog reference voltage(Volt) of the ADC
+#define TdsSensorPin 4
+#define VREF 3.3              // analog reference voltage(Volt) of the ADC
 #define SCOUNT  30            // sum of sample point
 
 int analogBuffer[SCOUNT];     // store the analog value in the array, read from ADC
@@ -9,7 +9,7 @@ int copyIndex = 0;
 
 float averageVoltage = 0;
 float tdsValue = 0;
-float temperature = 16;       // current temperature for compensation
+float temperature = 25;       // current temperature for compensation
 
 // median filtering algorithm
 int getMedianNum(int bArray[], int iFilterLen){
@@ -58,7 +58,7 @@ void loop(){
       analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
       
       // read the analog value more stable by the median filtering algorithm, and convert to voltage value
-      averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / 1024.0;
+      averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / 4096.0;
       
       //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0)); 
       float compensationCoefficient = 1.0+0.02*(temperature-25.0);
@@ -72,7 +72,7 @@ void loop(){
       //Serial.print(averageVoltage,2);
       //Serial.print("V   ");
       Serial.print("TDS Value:");
-      Serial.print(tdsValue);
+      Serial.print(tdsValue,0);
       Serial.println("ppm");
     }
   }
